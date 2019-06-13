e40bf055ea5246af55271e2ad8a7cbfe06b30c51
Holu8 <50380385+Holu8@users.noreply.github.com>
2019-06-13 13:55:34 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -171,3 +171,8 @@ function wpb_list_child_pages($pid) {
 
 
 ?>
+
+add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
+function enqueue_load_fa() {
+wp_enqueue_style( 'load-fa', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+}
